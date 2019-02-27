a5299b0d9c89a77d1783e8b97d7d85f9453cc0ef
Zhamilia Alimbekova <jamilysoul@mail.ru>
2019-02-27 12:50:36 +0300

    ref: add lists and dics

--- bikeshare.py
+++ bikeshare.py
@@ -4,7 +4,7 @@ import time
 import pandas as pd
 import numpy as np
 
-
+# Creation of the lists months, day_of_week and dictionaries CITY_DATA for further use 
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
